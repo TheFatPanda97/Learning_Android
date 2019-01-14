3047edfc63fc7e99f978b51dd1dff3df400d77ed
TheFatPanda97 <shawnhu97@gmail.com>
2019-01-13 23:02:04 -0500

    Initial commit

---
+++ app/src/main/java/ca/utoronto/shawnhu/learning_android/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.utoronto.shawnhu.learning_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
